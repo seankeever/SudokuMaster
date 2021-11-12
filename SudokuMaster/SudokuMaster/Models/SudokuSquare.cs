d4efd58429b505b0da5ac0d0dafd51a4833d076b
ravenlogistics\sean <sean@ravenlogistics.com>
2021-11-12 12:11:21 -0500

    Added code from local project

---
+++ SudokuMaster/SudokuMaster/Models/SudokuSquare.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuMaster.Models
+{
+    public class SudokuSquare
+    {
+        public SudokuSquare()
+        {
+            this.Value = 0;//set all to 0 initially then fill in based on text file
+            this.IsSet = false;
+            this.CanidateList = new CanidateList();
+        }
+
+        
+        public int SquareID { get; set; }
+        public bool IsSet { get; set; }//this is false until the square is set to a single digit
+        public int Value { get; set; }//blank or X value is represented by 0
+        public CanidateList CanidateList { get; set; }
+
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public int Region { get; set; }
+
+
+    }
+}
--- SudokuMaster/SudokuMaster/Program.cs
+++ SudokuMaster/SudokuMaster/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using SudokuMaster.Models;
+using SudokuMaster.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
 
 namespace SudokuMaster
 {
@@ -6,7 +12,38 @@ namespace SudokuMaster
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string puzzlePath = @"C:\Users\sean\source\repos\SudokuSolver\Puzzles\";
+            string puzzleName = "puzzle4.txt";
+            
+            SudokuGrid grid = LogicService.InitializePuzzle(puzzlePath,puzzleName);
+
+            int count = 0;
+            while (!grid.IsSolved)
+            {
+                LogicService.NarrowDownCanidates(ref grid);
+                while(LogicService.PopulateSingledOutCanidates(ref grid))
+                    ValidationService.CheckIfSolved(ref grid);
+                count++;
+                if (count == (9 * 81))
+                    LogicService.TakeAGuess(ref grid);
+            }
+            ImportExportService.ExportCompletedPuzzleSolution(grid, puzzlePath+puzzleName);
         }
+
     }
 }
+//static void Main(string[] args)
+//{
+//    string puzzlePath = @"C:\Users\sean\source\repos\SudokuSolver\Puzzles\";
+//    string puzzleName = "puzzle4.txt";
+
+//    SudokuGrid grid = LogicService.InitializePuzzle(puzzlePath, puzzleName);
+
+//    while (!grid.IsSolved)
+//    {
+//        LogicService.NarrowDownCanidates(ref grid);
+//        while (LogicService.PopulateSingledOutCanidates(ref grid))
+//            ValidationService.CheckIfSolved(ref grid);
+//    }
+//    ImportExportService.ExportCompletedPuzzleSolution(grid, puzzlePath + puzzleName);
+//}
